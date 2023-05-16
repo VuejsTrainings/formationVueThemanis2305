f968e60d4f9e5bc5fbade474a5096bf824bec549
Eric Guyader-Berger <ericguyader@yahoo.fr>
2023-05-16 15:18:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav>
+    <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+    <RouterLink :to="{ name: 'about' }">About</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+nav {
+  text-align: left;
+  margin-left: -1rem;
+  font-size: 1rem;
+
+  padding: 1rem 0;
+  margin-top: 1rem;
+}
+</style>
