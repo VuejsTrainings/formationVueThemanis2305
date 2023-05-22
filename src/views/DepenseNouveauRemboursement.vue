35bb9164df4a02e979d4eac1a01258a8a3a8fabf
Eric Guyader-Berger <ericguyader@yahoo.fr>
2023-05-22 23:37:35 +0200

    AJoute validation remboursement

--- src/views/DepenseNouveauRemboursement.vue
+++ src/views/DepenseNouveauRemboursement.vue
@@ -1,24 +1,35 @@
 <template>
     <h3>Saisie d'un remboursement</h3>
-    <form @submit.prevent="envoiRemboursement">
+    <VeeForm @submit="envoiRemboursement">
         <label for="date">Date</label>
-        <input id="date" type="date" v-model="date" />
+        <Field name="date" type="date" :rules="validateDate" />
+        <ErrorMessage name="date"></ErrorMessage> 
+        <br />
         
         <label for="montant">Montant</label>
-        <input id="montant" type="number" v-model="montant" />
+        <Field name="montant" type="number" :rules="validateMontant" />
+        <ErrorMessage name="montant"></ErrorMessage> 
+        <br />
         
         <label for="origine">Origine</label>
-        <select v-model="origine">
-            <option value="ro">Régime obligatoire</option>
+        <Field as="select" name="origine">
+            <option value="ro" selected>Régime obligatoire</option>
             <option value="mut">Mutuelle</option>
-        </select>
+        </Field>
+        <br />
         <button type="submit">OK</button>
-    </form>
+    </VeeForm>
 </template>
 <script>
 import { useDepensesStore } from '@/stores/depenses';
+import { Form, Field, ErrorMessage  } from 'vee-validate';
 
 export default {
+    components:{
+        Field,        
+        VeeForm: Form,
+        ErrorMessage 
+    },
     props: {
         depense: {
             type: Object, 
@@ -34,16 +45,32 @@ export default {
         }
     },
     methods:{
-        async envoiRemboursement(){
+        async envoiRemboursement(values){
             const remboursement = {
-                montant: this.montant,
-                date: this.date,
-                origine: this.origine,
+                montant: parseFloat(values.montant),
+                date: values.date,
+                origine: values.origine,
 
             };
             await useDepensesStore().ajouteRemboursement(this.depense.id, remboursement);
             this.modifie = false;            
             this.$router.replace({name:'depense-details'})   
+        },
+        validateMontant(value){
+            if( !value ) 
+                return 'Le montant doit être saisi'
+            if(value <= 0)
+                return 'Le montant doit être strictement positif'
+            if(value > this.depense.depense - this.depense.remboursements.reduce( (acc, cur) => acc += cur.montant, 0 ) )
+                return 'Le total des remboursements ne doit pas dépasser la dépense'
+            
+            return true
+        },
+        validateDate(value){
+            if( !value ) 
+                return 'Date obligatoire'
+            
+            return true
         }
     },
     beforeRouteLeave(){
