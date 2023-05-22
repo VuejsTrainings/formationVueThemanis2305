ae7943f98eecc74f96374e257fc435e1e7cb80c5
Eric Guyader-Berger <ericguyader@yahoo.fr>
2023-05-22 22:23:48 +0200

    Ajoute remboursements aux details

--- src/views/DetailDepense.vue
+++ src/views/DetailDepense.vue
@@ -2,6 +2,8 @@
     <p>{{ depense.intitule }}</p>
     <p class="finances">
         <span class="depense" v-if="depense.depense"> {{ depense.depense.toFixed(2) }} € </span>
+        <span class="remboursement" v-for="remb in depense.remboursements" :key="remb.id"> {{ remb.origine.toUpperCase() }} {{ remb.montant.toFixed(2) }} € </span>
+        <span class="rac" v-if="depense.depense"> {{ (depense.depense - depense.remboursements.reduce( (tot,cur) => tot += cur.montant , 0 )).toFixed(2) }} € </span>
     </p>
 </template>
 <script>
@@ -22,6 +24,21 @@ export default {
     color: #fff;
     background:#fa0;
     padding:0.4em;
+    margin-right: 1em;
+}
+.remboursement {
+    color: #fff;
+    background:#3a0;
+    padding:0.4em;
+    margin-right: 1em;
+}
+
+.rac {
+    color:#fff;
+    background:#f44;
+    padding:0.4em;
+    margin-right: 1em;
+    
 }
 
 </style>
